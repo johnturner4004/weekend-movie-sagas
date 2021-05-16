d9eaef5fdabd980a139b32ddad65ac1d1a0a22d3
johnturner4004 <johnturner4004@gmail.com>
2021-05-16 16:43:41 -0400

    added genres to the details page

--- server/routes/details.router.js
+++ server/routes/details.router.js
@@ -4,21 +4,15 @@ const pool = require('../modules/pool');
 
 router.post('/', (req, res) => {
   const id = req.body.id;
-  console.log('************************************************************************************************************************************************************************************************');
-
-  console.log('req.body:', req.body);
-
-  const queryString = `SELECT m.*, ARRAY_AGG(g.name) FROM movies AS m
+  
+  const queryString = `SELECT m.*, ARRAY_AGG(g.name) AS genre FROM movies AS m
   JOIN movies_genres AS mg ON m.id = mg.movie_id
   JOIN genres AS g ON mg.genre_id = g.id
   WHERE m.id=${id}
   GROUP BY m.id;`;
-  console.log(queryString);
 
   pool.query(queryString)
     .then(result => {
-      console.log(result);
-
       res.send(result.rows);
     })
     .catch(error => {
