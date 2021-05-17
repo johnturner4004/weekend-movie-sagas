513edbb2f3fc64450a35ae5750bb7f5aba045bc3
johnturner4004 <johnturner4004@gmail.com>
2021-05-16 22:33:34 -0400

    creat AddMovie.jsx and input elements

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+
+function AddMovie() {
+
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [details, setDetails] = useState('');
+
+  console.log(details);
+
+  function handleClick() {
+    console.log('click');
+  };
+
+  return(
+    <>
+    <h2>Enter movie details</h2>
+    <input type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} placeholder="title" required />
+    <input type="text" id="poster" value={url} onChange={(event) => setUrl(event.target.value)} placeholder="poster url" required /><br />
+    <textarea placeholder="movie description" onChange={(event) => setDetails(event.target.value)} rows="4" cols="36" required /><br />
+    <select required>
+      <optgroup>
+        <option disable selected value> -- Select a Genre -- </option>
+        {/* {
+        <option>test1</option>
+        } */}
+      </optgroup>
+    </select><br />
+    <button onClick={() => handleClick()}>Add Movie!</button>
+    </>
+  )
+}
+
+export default AddMovie;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import Details from '../Details/Details';
+import AddMovie from '../AddMovie/AddMovie'
 
 function App() {
   return (
@@ -16,10 +17,12 @@ function App() {
           <Details />
         </Route>
         {/* Add Movie page */}
+        <Route path="/add-movie">
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
 }
-
 
 export default App;
